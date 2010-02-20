71f9679b10d4b4e755fbd98ba1de7c1f2b2f7a20
Christian González <christiangda@gmail.com>
2010-02-20 01:07:08 +0000

    Agregue los archivos de descripción para OpenOffice

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-
+#
 # Descomentar la linea de abajo si se quiere debugear
 #set -x
 
@@ -9,16 +9,6 @@ TMP_DIR=build
 
 # El archivo de configuración por defecto es config.sh
 # Las variables establecidas en el archivo config.sh son:
-APP_NAME=          # short-name, jar and xpi files name. Must be lowercase with no spaces
-CHROME_PROVIDERS=  # which chrome providers we have (space-separated list)
-CLEAN_UP=          # delete the jar / "files" when done?       (1/0)
-ROOT_FILES=        # put these files in root of xpi (space separated list of leaf filenames)
-ROOT_DIRS=         # ...and these directories       (space separated list)
-VAR_FILES=         # files that need variable substitution (space separated list)
-VERSION=           # version number (string)
-PRUNE_DIRS=	       # exclude files with these directories in their paths (space separated list)
-BEFORE_BUILD=      # run this before building       (bash command)
-AFTER_BUILD=       # ...and this after the build    (bash command)
 
 # Incluimos el archivo de configuración
 . ./config.sh
