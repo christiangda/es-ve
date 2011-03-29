69f106a519a4efe47326f880754a5e24ebeba1f4
christiangda <christiangda@50d6c021-a183-4cb3-a019-0b1d5bcebc6a>
2011-03-29 20:45:43 +0000

    Cambie la estructura del repositorio

---
+++ trunk/apps/Mozilla/install.js
@@ -0,0 +1,15 @@
+const APP_DESCRIPTION	= "Diccionario español Venezuela";
+const APP_AUTHOR		= "christiangda@gmail.com";
+const APP_VERSION		= "1.0.4";
+
+var err = initInstall(APP_DESCRIPTION, APP_AUTHOR, APP_VERSION);
+if (err != SUCCESS)
+    cancelInstall();
+
+var fProgram = getFolder("Program");
+err = addDirectory("", APP_AUTHOR, "dictionaries", fProgram, "dictionaries", true);
+
+if (err != SUCCESS)
+    cancelInstall();
+
+performInstall();
