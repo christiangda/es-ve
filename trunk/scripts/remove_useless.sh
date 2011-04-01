0d1d3236fba76f0ec2375b2a22ed1be0ef9e56ef
christiangda <christiangda@50d6c021-a183-4cb3-a019-0b1d5bcebc6a>
2011-04-01 03:51:26 +0000

    agregue script para eliminar comentarios, espacios vacios, lineas vacias y con tabuladores y tabuladoes.  Adicionalmente ordena el archvo y elimina lineas duplicadas

---
+++ trunk/scripts/remove_useless.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+#
+# Elimina espacios en blanco, tabuladores y lineas que comienzen con el 
+# caracter "#", lo cual significa comentarios.  Adicionalmente elimina
+# espacios y tabuladores al final de cada linea y ordena en orden alfabético
+# las cadenas pasadas como entrada y elimina los duplicados
+#
+# USO:  cat fichero_entrada | remove_useless.sh > fichero_salida
+#
+sed '/^\#/d;/./!d;s/^[ \t]*//;s/[ \t]*$//' | sort | uniq
