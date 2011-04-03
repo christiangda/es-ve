7f765c5b04841bfeba9a2cb0be34f35308d2fe73
Christian González <christiangda@gmail.com>
2011-04-03 07:50:22 +0000

    Agregue los diccionarios v1.0.4 y las nuevas versiones de los scripts build.sh y config.sh

--- build.sh
+++ build.sh
@@ -8,30 +8,8 @@
 #Variables de ubicación
 ROOT_DIR=`pwd`
 TMP_DIR="build"
+SVN=0
 
-
-MKDIR=`which mkdir 2>/dev/null`
-GREP=`which grep 2>/dev/null`
-FIND=`which find 2>/dev/null`
-ZIP=`which zip 2>/dev/null`
-
-# Abandonar si no se encuentra alguna de las herramientas
-if [ "$MKDIR" == "" ]; then
-	echo "No se encontró el comando 'mktemp'... Abortando." > /dev/stderr
-	exit 1
-fi
-if [ "$GREP" == "" ]; then
-	echo "No se encontró el comando 'grep'... Abortando." > /dev/stderr
-	exit 1
-fi
-if [ "$FIND" == "" ]; then
-	echo "No se encontró el comando 'find'... Abortando." > /dev/stderr
-	exit 1
-fi
-if [ "$ZIP" == "" ]; then
-	echo "No se encontró el comando 'zip'... Abortando." > /dev/stderr
-	exit 1
-fi
 
 # Obtener las variables de la configuración del archivo config.sh
 if [ -f ./config.sh ]; then
@@ -39,6 +17,11 @@ if [ -f ./config.sh ]; then
 else
 	echo "No se encontró el archivo 'config.sh'... Abortando." > /dev/stderr
 	exit 1
+fi
+
+#Valido si estamos trabajando dentro de un repositorio local de Subversion o no
+if [ -d ".svn" ]; then
+	SVN=1
 fi
 
 #Verificamos si se quieren crear los diccionarios
@@ -52,10 +35,12 @@ if [ "$PKG_MOZ_CREATE" == "0" -a "$PKG_oOo_CREATE" == "0" ]; then
 	exit 1
 fi
 
+
+
+
 # Hacemos una copia del valor de la variable LANG para poder restaurarla.
 LANG_BAK=$LANG
-
-LANG="$LOCALIZACION.ISO-8859-1"
+LANG=$LOCALIZACION.$ENCODING
 
 # Obtener el archivo de afijos
 if [ -f $ROOT_DIR/src/affixes/${AFFIXES_FILE_NAME} ]; then
@@ -98,10 +83,10 @@ if [ -d $BUILD_DIR ]; then
 	rm -rf $BUILD_DIR
 fi
 
-$MKDIR $ROOT_DIR/$TMP_DIR
+mkdir $ROOT_DIR/$TMP_DIR
 
 
-#Copiamos todos los archivos necesarios a esta nueva ruta y les removemos los
+#Copio todos los archivos necesarios a esta nueva ruta y les remuevo los
 #comentarios y las lineas en blanco
 cat $AFFIXES_FILE | $ROOT_DIR/scripts/remove_useless.sh > $BUILD_DIR/${AFFIXES_FILE_NAME}
 cat $HYPHNATION_FILE | $ROOT_DIR/scripts/remove_useless.sh > $BUILD_DIR/${HYPHNATION_FILE_NAME}
@@ -109,18 +94,130 @@ cat $THESAURUS_DAT_FILE | $ROOT_DIR/scripts/remove_useless.sh > $BUILD_DIR/${THE
 cat $THESAURUS_IDX_FILE | $ROOT_DIR/scripts/remove_useless.sh > $BUILD_DIR/${THESAURUS_IDX_FILE_NAME}
 
 #Reusamos las mismas variables
+DICTIONARY_FILE=$BUILD_DIR/${DICTIONARY_FILE_NAME}
 AFFIXES_FILE=$BUILD_DIR/${AFFIXES_FILE_NAME}
 HYPHNATION_FILE=$BUILD_DIR/${HYPHNATION_FILE_NAME}
 THESAURUS_DAT_FILE=$BUILD_DIR/${THESAURUS_DAT_FILE_NAME}
 THESAURUS_IDX_FILE=$BUILD_DIR/${THESAURUS_IDX_FILE_NAME}
 
-#Creamos el diccionario de los toponimos
+#Creo el diccionario de los toponimos
 cat $ROOT_DIR/src/words/toponyms/*.txt \
 	| $ROOT_DIR/scripts/remove_useless.sh \
 	| $ROOT_DIR/scripts/order_remove_duplicates.sh \
 	> $BUILD_DIR/'TOPONYMS_'${DICTIONARY_FILE_NAME}'.txt'
 
+#Creo el archivo que contiene las palabras RAE y NoRAE
+cat $ROOT_DIR/src/words/NoRAE/*.txt \
+	$ROOT_DIR/src/words/RAE/*.txt \
+	| $ROOT_DIR/scripts/remove_useless.sh \
+	| $ROOT_DIR/scripts/order_remove_duplicates.sh \
+	> $BUILD_DIR/'NoRAE_AND_RAE_'${DICTIONARY_FILE_NAME}'.txt'
+
+#Creo el archivo del diccionario
+touch $DICTIONARY_FILE
+
+cat $BUILD_DIR/'TOPONYMS_'${DICTIONARY_FILE_NAME}'.txt' \
+	$BUILD_DIR/'NoRAE_AND_RAE_'${DICTIONARY_FILE_NAME}'.txt' \
+	| $ROOT_DIR/scripts/order_remove_duplicates.sh \
+	| wc -l >> $DICTIONARY_FILE
+	
+cat $BUILD_DIR/'TOPONYMS_'${DICTIONARY_FILE_NAME}'.txt' \
+	$BUILD_DIR/'NoRAE_AND_RAE_'${DICTIONARY_FILE_NAME}'.txt' \
+	| $ROOT_DIR/scripts/order_remove_duplicates.sh \
+	>> $DICTIONARY_FILE
+
+#Si estoy dentro de un repositorio de Subversion, se deben exportar
+#las carpetas de las aplicaciones
+if [ "$SVN" == "1" ]; then
+	svn export $ROOT_DIR/apps $BUILD_DIR/apps > /dev/null 2>&1
+else
+	cp -r -P $ROOT_DIR/apps $BUILD_DIR/
+fi
+
+#Reemplazo las Variables definidas en config.sh en los respectivos
+#archivos dentro de las aplicaciones
+
+if [ "$PKG_MOZ_CREATE" == "1" ]; then
+	echo "Creando diccionario para Mozilla ..."
+
+	for PKG_VAR in `grep -e 'PKG_MOZ_*' $ROOT_DIR/config.sh`
+	do
+		KEY=`echo ${PKG_VAR} | gawk -F= '{ print $1 }'`
+		VALUE=`echo ${PKG_VAR} | gawk -F= '{ print $2 }' | sed -e 's/\"//g'`
 
 
+		for APPS_FILE in `grep -R $KEY $BUILD_DIR/apps/Mozilla/ | gawk -F: '{ print $1 }' | sort | uniq`
+		do
+			sed -i.bkup s/${KEY}/${VALUE}/g $APPS_FILE
+			rm -rf $APPS_FILE'.bkup'
+		done
+	done
+	
+	#Coloco los diccionarios y demás cosas donde deben ir
+	cp $DICTIONARY_FILE $BUILD_DIR/apps/Mozilla/dictionaries/
+	cp $AFFIXES_FILE $BUILD_DIR/apps/Mozilla/dictionaries/
+	
+	#Creo el archivo comprimido del diccionario
+	cd $BUILD_DIR/apps/Mozilla/
+	zip -r -q $PKG_MOZ_NAME_FILE *
+	OUT_VER=`grep -e 'PKG_MOZ_VERSION' $ROOT_DIR/config.sh | gawk -F= '{ print $2 }' | sed -e 's/\"//g'`
+	OUT_DIR=$ROOT_DIR'/dictionaries/Mozilla/v'$OUT_VER
+	
+	if [ ! -d $OUT_DIR ]; then
+		mkdir $OUT_DIR
+	else
+		echo '¡Alerta!'
+		echo "Será reemplazado el archivo $OUT_DIR/$PKG_MOZ_NAME_FILE"
+	fi
+	
+	cp ./$PKG_MOZ_NAME_FILE $OUT_DIR/
+	cd $ROOT_DIR
+fi
+
+if [ "$PKG_oOo_CREATE" == "1" ]; then
+	echo "Creando diccionario para OpenOffice ..."
+
+	for PKG_VAR in `grep -e 'PKG_oOo_*' $ROOT_DIR/config.sh`
+	do
+		KEY=`echo -n ${PKG_VAR} | gawk -F= '{ print $1 }'`
+		VALUE=`echo -n ${PKG_VAR} | gawk -F= '{ print $2 }' | sed -e 's/\"//g'`
+
+		for APPS_FILE in `grep -R $KEY $BUILD_DIR/apps/OpenOffice/ | gawk -F: '{ print $1 }' | sort | uniq`
+		do
+			sed -i.bkup s/${KEY}/${VALUE}/g $APPS_FILE
+			rm -rf $APPS_FILE'.bkup'
+		done
+	done
+	
+	#Coloco los diccionarios y demás cosas donde deben ir
+	#Coloco los diccionarios y demás cosas donde deben ir
+	cp $DICTIONARY_FILE $BUILD_DIR/apps/OpenOffice/dictionaries/
+	cp $AFFIXES_FILE $BUILD_DIR/apps/OpenOffice/dictionaries/
+	cp $HYPHNATION_FILE $BUILD_DIR/apps/OpenOffice/dictionaries/
+	cp $THESAURUS_DAT_FILE $BUILD_DIR/apps/OpenOffice/dictionaries/
+	cp $THESAURUS_IDX_FILE $BUILD_DIR/apps/OpenOffice/dictionaries/
+	
+	#Creo el archivo comprimido del diccionario
+	cd $BUILD_DIR/apps/OpenOffice/
+	zip -r -q $PKG_oOo_NAME_FILE *
+	OUT_VER=`grep -e 'PKG_oOo_VERSION' $ROOT_DIR/config.sh | gawk -F= '{ print $2 }' | sed -e 's/\"//g'`
+	OUT_DIR=$ROOT_DIR'/dictionaries/OpenOffice/v'$OUT_VER
+	
+	if [ ! -d $OUT_DIR ]; then
+		mkdir $OUT_DIR
+	else
+		echo '¡Alerta!'
+		echo "Será reemplazado el archivo $OUT_DIR/$PKG_oOo_NAME_FILE"
+	fi
+	
+	cp ./$PKG_oOo_NAME_FILE $OUT_DIR/
+	cd $ROOT_DIR
+	
+fi
+
+#Eliminamos la carpeta temporal de construcción
+rm -rf build/
+
+LANG=$LANG_BAK
 
 exit 0
--- config.sh
+++ config.sh
@@ -1,45 +1,56 @@
 
-LOCALIZACION='es_VE'
-ENCODING='utf8'
+LOCALIZACION="es_VE"
+ENCODING="utf8"
 
-DICTIONARY_FILE_NAME='es_VE.dic'
-AFFIXES_FILE_NAME='es_VE.aff'
-HYPHNATION_FILE_NAME='hyph_es_VE.dic'
-THESAURUS_DAT_FILE_NAME='th_es_VE.dat'
-THESAURUS_IDX_FILE_NAME='th_es_VE.idx'
+DICTIONARY_FILE_NAME="es_VE.dic"
+AFFIXES_FILE_NAME="es_VE.aff"
+HYPHNATION_FILE_NAME="hyph_es_VE.dic"
+THESAURUS_DAT_FILE_NAME="th_es_VE.dat"
+THESAURUS_IDX_FILE_NAME="th_es_VE.idx"
 
 ###############################################################################
 # Mozilla
 ###############################################################################
+
 # ¿Crear este diccionario?, 1 si se quiere crear, 0 si no
-PKG_MOZ_CREATE=0
+PKG_MOZ_CREATE=1
+
+#Identificador único del la extensión para Mozilla
+PKG_MOZ_ID="es-ve@dictionaries.addons.mozilla.org"
+
+#Nombre que tendrá el archivo del paquete del diccionario para Mozilla
+PKG_MOZ_NAME_FILE="diccionario_espa__ol_venezuela-1.0.4-fx+tb+sb+sm.xpi"
 
 # Version
-PKG_MOZ_VERSION='1.0.4'
+PKG_MOZ_VERSION="1.0.4"
 
 # Mozilla Firefox
-PKG_MOZ_FF_MIN_VER='3.0'
-PKG_MOZ_FF_MAX_VER='4.2a1pre'
+PKG_MOZ_FF_MIN_VER="3.0"
+PKG_MOZ_FF_MAX_VER="4.2a1pre"
 
 # Mozilla Thunderbird
-PKG_MOZ_TB_MIN_VER='3.0'
-PKG_MOZ_TB_MAX_VER='3.3a4pre'
+PKG_MOZ_TB_MIN_VER="3.0"
+PKG_MOZ_TB_MAX_VER="3.3a4pre"
 
 # Mozilla Sunbird
-PKG_MOZ_SB_MIN_VER='0.4a1'
-PKG_MOZ_SB_MAX_VER='1.0pre'
+PKG_MOZ_SB_MIN_VER="0.4a1"
+PKG_MOZ_SB_MAX_VER="1.0pre"
 
 # Mozilla SeaMonkey
-PKG_MOZ_SM_MIN_VER='2.0'
-PKG_MOZ_SM_MAX_VER='2.2a1pre'
+PKG_MOZ_SM_MIN_VER="2.0"
+PKG_MOZ_SM_MAX_VER="2.2a1pre"
 
 
 ###############################################################################
 # OpenOffice
 ###############################################################################
+
 # ¿Crear este diccionario?, 1 si se quiere crear, 0 si no
-PKG_oOo_CREATE=0
+PKG_oOo_CREATE=1
+
+#Nombre que tendrá el paquete del diccionario para OpenOffice
+PKG_oOo_NAME_FILE="dict-es_VE.oxt"
 
 # Version
-PKG_oOo_VERSION='1.0.4'
+PKG_oOo_VERSION="1.0.4"
 
