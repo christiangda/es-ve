8c1097abbb3a59b99900deec9aa97c549a0a1064
christiangda <christiangda@50d6c021-a183-4cb3-a019-0b1d5bcebc6a>
2011-08-06 12:45:26 +0000

    Cree la versión 1.1.6

--- trunk/apps/Mozilla/install.js
+++ trunk/apps/Mozilla/install.js
@@ -1,13 +1,13 @@
-const APP_DESCRIPTION	= "PKG_MOZ_NAME";
-const APP_AUTHOR		= "PKG_MOZ_AUTHOR_NAME <PKG_MOZ_AUTHOR_EMAIL>";
-const APP_VERSION		= "PKG_MOZ_VERSION";
+const ES_VE_APP_DESCRIPTION	= "PKG_MOZ_NAME";
+const ES_VE_APP_AUTHOR		= "PKG_MOZ_AUTHOR_NAME <PKG_MOZ_AUTHOR_EMAIL>";
+const ES_VE_APP_VERSION		= "PKG_MOZ_VERSION";
 
-var err = initInstall(APP_DESCRIPTION, APP_AUTHOR, APP_VERSION);
+var err = initInstall(ES_VE_APP_DESCRIPTION, ES_VE_APP_AUTHOR, ES_VE_APP_VERSION);
 if (err != SUCCESS)
     cancelInstall();
 
-var fProgram = getFolder("Program");
-err = addDirectory("", APP_AUTHOR, "dictionaries", fProgram, "dictionaries", true);
+var ES_VE_fProgram = getFolder("Program");
+err = addDirectory("", ES_VE_APP_AUTHOR, "dictionaries", ES_VE_fProgram, "dictionaries", true);
 
 if (err != SUCCESS)
     cancelInstall();
